9b3cb58bc4b9c89b0806143b453744c317d911fa
Clement G <contact@clemdesign.fr>
2020-08-13 16:24:30 +0200

    - Fix translation issue

--- src/app/choice/choice.page.ts
+++ src/app/choice/choice.page.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoicePage implements OnInit {
 
-  constructor() { }
+  date: Date;
+
+  constructor() {
+    this.date = new Date();
+  }
 
   ngOnInit() {
   }
